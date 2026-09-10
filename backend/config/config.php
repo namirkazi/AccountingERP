473dd0fbecfd0e9dbb56132662546a7260d59f2f
namirkazi <namir2004@gmail.com>
2026-09-10 18:29:36 +0400

    fixign database issue

--- backend/config/config.php
+++ backend/config/config.php
@@ -2,9 +2,17 @@
 
 header('Content-Type: application/json');
 
+$isHttps = (
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+);
+
 session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'secure' => $isHttps,
     'httponly' => true,
-    'samesite' => 'Lax'
+    'samesite' => 'None'
 ]);
 
 session_start();
