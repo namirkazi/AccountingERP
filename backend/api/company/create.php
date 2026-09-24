0e9e239d4fb599aa07ba7fdec5a20a83f9702699
namirkazi <namir2004@gmail.com>
2026-09-24 15:26:29 +0400

    Fixed create new company issue

--- backend/api/company/create.php
+++ backend/api/company/create.php
@@ -1,8 +1,11 @@
 <?php
 
+require_once __DIR__ . '/../../config/cors.php';
+
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../models/User.php';
+
 
 header('Content-Type: application/json');
 
